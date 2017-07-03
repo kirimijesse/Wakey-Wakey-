bc9ad018f84462084e07ce88531d7b8b15a685d6
Jesse Kirimi <rimijessse@gmail.com>
2017-07-03 23:58:47 +0300

    comments

--- alarm.py
+++ alarm.py
@@ -1,5 +1,7 @@
 import tkinter
-import winsound 
+import winsound #provides access to the basic sound-playing machinery provided by Windows platforms
 import time 
 import math 
 
+def countdown:
+	
